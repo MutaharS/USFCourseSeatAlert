a9078bf7c48bd6ebe220b6ee440b2250bbcb0d4b
MutaharS <mutahar014@gmail.com>
2021-04-06 19:54:26 -0700

    Add files via upload

---
+++ courseAvailable.py
@@ -0,0 +1,34 @@
+import requests, time
+from bs4 import BeautifulSoup
+import smtplib, ssl
+
+port = 465  # For SSL
+password = "password"
+message = """\
+Subject: COURSE SEAT UPDATE
+
+CEN 4020, CRN 57429 SEAT AVAILABLE!"""
+
+print("This version works for Summer 2021 Classes")
+crn = raw_input("Please enter the course's CRN#")
+URL = 'https://usfonline.admin.usf.edu/pls/prod/bwckschd.p_disp_detail_sched?term_in=202105&crn_in='+ crn
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+flag = 0
+while(flag == 0):
+    table_elems = soup.find('table', class_='datadisplaytable')
+    table_data = table_elems.find_all('td', class_='dddefault')
+    numSeatsAvailable = str(table_data[3])
+    numSeatsAvailable = numSeatsAvailable[22:23]
+    numSeatsAvailable = int(numSeatsAvailable)
+    if(numSeatsAvailable > 0):
+        print("THERE IS SEAT AVAILABLE!")
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
+        server.login("courseSeatAvailable@gmail.com", password)
+        server.sendmail("courseSeatAvailable@gmail.com", "mutahar@usf.edu", message)
+        flag = 1
+        break
+    time.sleep(1)
